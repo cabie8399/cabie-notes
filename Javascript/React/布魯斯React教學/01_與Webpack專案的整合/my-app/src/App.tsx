eca2b2bb193f82720811126f0c1948c72a5fb3b9
Cabie Wu <peterjuju99@gmail.com>
2024-05-24 01:44:19 +0800

    [Note] React教材資源

--- Javascript/React/布魯斯React教學/01_與Webpack專案的整合/my-app/src/App.tsx
+++ Javascript/React/布魯斯React教學/01_與Webpack專案的整合/my-app/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+// function寫法
+// function App() {
+//   return (
+//     <div className="App">
+//       cabie
+//     </div>
+//   );
+// }
+
+// ts寫法
+const App: React.FC = () => {
   return (
     <div className="App">
-      cabie
+      Pizza
     </div>
   );
 }
