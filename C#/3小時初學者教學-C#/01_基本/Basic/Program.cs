9ffa26ac7a67d258af0e7bd5732b5d45de793079
Cabie Wu <peterjuju99@gmail.com>
2024-11-05 21:18:45 +0800

    [C#] 基本語法

---
+++ C#/3小時初學者教學-C#/01_基本/Basic/Program.cs
@@ -0,0 +1,39 @@
+﻿// Hello World
+ Console.WriteLine("Hello, World!");
+
+
+// 資料型態
+// 字串、字元、整數、浮點數、布林值
+//string name = "Pizza";
+//char sex = 'M';
+//int age = 10;
+//double weight = 3.1;
+//bool is_Maltese = true;
+
+//Console.WriteLine(name);
+//Console.WriteLine(sex);
+
+
+// 字串
+// 字串長度
+//string name = "Hello Pizza";
+//Console.WriteLine(name.Length);
+//// 是否有某字串
+//Console.WriteLine(name.Contains("Hello"));
+//// 字串切割
+//Console.WriteLine(name.Substring(2));
+//Console.WriteLine(name.Substring(2, 3));
+
+
+// 數字
+//Console.WriteLine(System.Math.Abs(-10.2));
+//Console.WriteLine(System.Math.Pow(2,3));
+//Console.WriteLine(System.Math.Sqrt(64));
+//Console.WriteLine(System.Math.Max(2,100));
+//Console.WriteLine(System.Math.Round(3.6));
+
+
+// 用戶輸入
+//Console.Write("請輸入狗狗的名字: ");
+//String name = Console.ReadLine();
+//Console.WriteLine("你好!" + name);
